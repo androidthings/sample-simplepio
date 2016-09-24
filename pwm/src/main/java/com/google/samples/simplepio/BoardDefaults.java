8b1a1948fbd1d13e9a69fdf58496a9edd17a85a0
Renato Mangini <mangini@google.com>
2016-09-23 18:27:06 -0700

    Add PWM simplepio sample
    Change-Id: I5ea2099ec4f9020cc08b095719af0f953d6eca08

---
+++ pwm/src/main/java/com/google/samples/simplepio/BoardDefaults.java
@@ -0,0 +1,27 @@
+package com.google.samples.simplepio;
+
+import android.os.Build;
+
+@SuppressWarnings("WeakerAccess")
+public class BoardDefaults {
+    private static final String DEVICE_EDISON = "edison";
+    private static final String DEVICE_RPI3 = "rpi3";
+    private static final String DEVICE_NXP = "nxp";
+
+    /**
+     * Return the preferred PWM port for each board.
+     */
+    public static String getPWMPort() {
+        // TODO: confirm DEVICE and preferred port for RPI3 and NXP
+        switch (Build.DEVICE) {
+            case DEVICE_EDISON:
+                return "IO6";
+            case DEVICE_RPI3:
+                return "26";
+            case DEVICE_NXP:
+                return "26";
+            default:
+                throw new IllegalStateException("Unknown Build.DEVICE " + Build.DEVICE);
+        }
+    }
+}
